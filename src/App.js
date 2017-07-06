209b3867e62d8501751830fb98d0a1b06e84a30f
Julio Imazio Moyano <imazio@gmail.com>
2017-07-06 20:27:49 +0100

    feat: Moving component to a functional one, with a separated state
    Not sure if i am understanding it right. Practicing GIT best practices as well.
    
    - Move component to a separated file, BookShelf.js
    - Deciding how the data has to flow to it

--- src/App.js
+++ src/App.js
@@ -1,21 +1,97 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import * as BooksAPI from './BooksAPI'
+import './App.css'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+class Bookshelf extends React.Component {
+  render () {
+
+    const books = [
+      {
+        title: 'El pendulo de foucault',
+        authors: 'Umberto Eco',
+        imageLinks: {smallThumbnail: 'http://books.google.com/books/content?id=nggnmAEACAAJ&printsec=frontcover&img=1&zoom=5&source=gbs_api'},
+        shelf: 'currentlyReading',
+        id: '32sdfs'
+      },
+      {
+        title: 'Libro dos',
+        authors: 'Umberto Eco',
+        imageLinks: {smallThumbnail: 'http://books.google.com/books/content?id=nggnmAEACAAJ&printsec=frontcover&img=1&zoom=5&source=gbs_api'},
+        shelf: 'wantToRead',
+        id: '323sd'
+      },
+      {
+        title: 'Libro tres',
+        authors: 'Umberto Eco',
+        imageLinks: {smallThumbnail: 'http://books.google.com/books/content?id=nggnmAEACAAJ&printsec=frontcover&img=1&zoom=5&source=gbs_api'},
+        shelf: 'read',
+        id: '32323'
+      }
+    ]
+
+    return <div className="bookshelf">
+      <h2 className="bookshelf-title">{this.props.bookshelf_title}</h2>
+      <div className="bookshelf-books">
+        <ol className="books-grid">
+          {books.filter(book => book.shelf === this.props.category).map(book => (
+            <li key={book.id}>
+              <div className="book">
+                <div className="book-top">
+                  <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url({book.imageLinks.smallThumbnail})' }}></div>
+                  <div className="book-shelf-changer">
+                    <select>
+                      <option value="none" disabled>Move to...</option>
+                      <option value="currentlyReading">Currently Reading</option>
+                      <option value="wantToRead">Want to Read</option>
+                      <option value="read">Read</option>
+                      <option value="none">None</option>
+                    </select>
+                  </div>
+                </div>
+                <div className="book-title">{book.title}</div>
+                <div className="book-authors">book.authors</div>
+              </div>
+            </li>
+          ))}
+        </ol>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+    </div>
   }
 }
 
-export default App;
+class BooksApp extends React.Component {
+  state = {
+    /**
+     * TODO: Instead of using this state variable to keep track of which page
+     * we're on, use the URL in the browser's address bar. This will ensure that
+     * users can use the browser's back and forward buttons to navigate between
+     * pages, as well as provide a good URL they can bookmark and share.
+     */
+    showSearchPage: false
+  }
+
+
+  render() {
+    return (
+      <div className="app">
+          <div className="list-books">
+            <div className="list-books-title">
+              <h1>MyReads</h1>
+            </div>
+            <div className="list-books-content">
+              <div>
+                <Bookshelf category='currentlyReading' bookshelf_title='Currently Reading'/>
+                <Bookshelf category='wantToRead' bookshelf_title='Want to Read'/>
+                <Bookshelf category='read' bookshelf_title='Read'/>
+              </div>
+            </div>
+            <div className="open-search">
+              <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
+            </div>
+          </div>
+      </div>
+    )
+  }
+}
+
+export default BooksApp
