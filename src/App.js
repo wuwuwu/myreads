af2eb81f3216004d29234f8964b1a5ac27b98024
Julio Imazio Moyano <imazio@gmail.com>
2017-07-26 12:39:20 +0100

    Updating a book in the app calls the API

--- src/App.js
+++ src/App.js
@@ -15,30 +15,11 @@ class BooksApp extends React.Component {
     newbooks: []
   }
   /*
-  TO DO
 
-  // [] Refactor to have a Book component. Verify and operate over the book ditionary itself in the component
-  // [] Add logic to show the number of books in each category
-  // [] Handle the delay when updating books (changing shelfs), update local data directly instead of doing after updating with api put.then `BooksAPI.update` . if `success` do nothing, if `error` rollback the local change
-  // [] Learn how to create regresive tests for each keyword in the search
-
-  // X Solve problem changing an existing book from the search view, creating a duplicated book
-  // X Add feature: always order books alphabetically and scape characters
-  // X Not all books have a Thumbnail, imageLinks or Authors, create a logic to use default values in that case (null or undefined)
-  // X In the search query, check if it has changed and only make the call if it has indeed changed
-  // X Add Logic for search showing actual api returned books
-  // X Find out why changes in search do not update the Search view
-  // X Add conditional, if the books exists, change the shelf value
-  // X If it does not exists, add it to the state
-  // X Clean the code from console calls
-  // X Make a call updating the API when the values of the book are changed
-  // X Add a rule to check if the id actually exists in the array
-  // X Add logic to change shelf of the book even in the search
-  // X Add Logic to show in book sub menu the current category of the book
-  // X Show different homepage and search pages | Adding routes
-  // X Add a shelf foor each category
-  // X Add logic to change the shelf title acordingly with the category
-
+  // TODO: [] Refactor to have a Book component. Verify and operate over the book ditionary itself in the component
+  // TODO: [] Add logic to show the number of books in each category
+  // TODO: [] Handle the delay when updating books (changing shelfs), update local data directly instead of doing after updating with api put.then `BooksAPI.update` . if `success` do nothing, if `error` rollback the local change
+  // TODO: [] Learn how to create regresive tests for each keyword in the search
   */
 
   updateBook = (book, evt) => {
@@ -48,11 +29,13 @@ class BooksApp extends React.Component {
       return b.id
     })
     booksKeys.includes(book.id) ? (
-      this.setState((state) => ({ books: this.state.books.map((b) => { return b.id === book.id ? (b.shelf = shelf, b) : (b)}).sort(sortBy('title'))}))
+      this.setState((state) => ({ books: this.state.books.map((b) => { return b.id === book.id ? (b.shelf = shelf, b) : (b)}).sort(sortBy('title'))})),
+      BooksAPI.update(book, shelf).then(() => {})
     ):(
       book.shelf = shelf,
       newbooks.push(book),
-      this.setState({ books: newbooks.sort(sortBy('title')) })
+      this.setState({ books: newbooks.sort(sortBy('title')) }),
+      BooksAPI.update(book, shelf).then(() => {})
     )
   }
 
